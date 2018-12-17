4d6cd1a26e4d4c3950de775346da33a426a304c6
berkay <berkayinan@gmail.com>
2018-12-17 13:53:25 +0100

    added script to init togglew

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ app = Flask(__name__)
 
 
 
-@app.route('/')
+@app.route('/temp')
 def hello_world():
-    return render_template('main.html')
+    return render_template('/index.md')
 
+@app.route('/')
+def source_sep():
+    return render_template('/source_sep.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
